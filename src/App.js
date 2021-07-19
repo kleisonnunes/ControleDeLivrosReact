1570e0cd08620fb11ca5175683d4f10190c7a537
Kleison Nunes <kleison.nunes00@gmail.com>
2021-07-19 17:25:04 -0300

    Primeiro Componente

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Teste from './Componentes/teste';
+
+function App() {
+  return (
+    <Teste />
+  );
+}
+
+export default App;
---
+++ src/Componentes/teste.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div align="center">
+                <h2>Meu primeiro Componente - Kleison</h2>
+                <img src="https://s2.glbimg.com/H7rjzaqCQgEyz9ufwSzmYxQfzMA=/0x0:2048x1314/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/B/4/CpdjQ4SZANm1zQK9rIBw/pordosol.jpg" alt="Imagem da natureza" height="580" width="830"/>  
+            </div>
+        );
+    }
+}
+
+export default Teste;
